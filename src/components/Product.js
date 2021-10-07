9156ff83ed2711dc6283571368e34a3a60e8d55f
Michael Tong <michaelyuyangtong@gmail.com>
2021-10-07 19:33:04 +0800

    Added new logos and landing page svg

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,12 +5,13 @@ import {
   SiTailwindcss,
   SiElectron,
   SiFirebase,
+  SiPython,
 } from "react-icons/si";
 
 export default function Product() {
   return (
-    <div className="">
-      <div className="mx-auto container p-8 flex flex-col-reverse lg:flex-row">
+    <div className="mx-auto container p-8">
+      <div className="flex flex-col-reverse lg:flex-row">
         <div className="z-20 mt-8 lg:my-auto">
           <div className="w-full lg:w-[380px] xl:w-[460px]">
             <h1 className="my-4 text-5xl xl:text-6xl font-bold tracking-tight leading-[3.25rem]">
@@ -37,12 +38,31 @@ export default function Product() {
           </div>
         </div>
         <img
-          src="focaltimer-banner.png"
+          // src="focaltimer-banner.png"
+          src="focaltimer-landing.svg"
           alt="FocalTimer Banner"
           className="md:flex-auto lg:w-1/2 xl:flex-auto lg:ml-4 -mr-4"
         />
       </div>
-      <div className="mx-auto container px-8"></div>
+      <hr className="w-full h-0.5 border-none bg-gray-100 my-8" />
+      <div>
+        <h2 className="text-3xl font-bold">Versions:</h2>
+        <div className="flex gap-8 mt-4">
+          <div className="w-full h-32 border-gray-100 hover:border-gray-600 border-2 rounded-md flex flex-col text-center p-8">
+            <h3 className="text-2xl font-bold mb-2">FocalTimer Classic</h3>
+            <p className="text-gray-500 text-sm leading-4">
+              All the features you need in one lightweight package
+            </p>
+          </div>
+          <div className="w-full h-32 border-gray-100 hover:border-gray-600 border-2 rounded-md flex flex-col text-center p-8">
+            <h3 className="text-2xl font-bold mb-2">FocalTimer V2</h3>
+            <p className="text-gray-500 text-sm leading-4">
+              Feature packed workspace management tool running straight in your
+              browser
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
